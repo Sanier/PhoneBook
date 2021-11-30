2387ddabf3f931b71f13c398557febdb3222e53c
Sanier <Sanier20@mail.ru>
2021-11-30 12:53:55 +0300

    Сортировка по имени перед циклом

--- Program.cs
+++ Program.cs
@@ -18,29 +18,25 @@ namespace PhoneBook
             phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
             phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
 
+            var sortedName = phoneBook.OrderBy(s => s.Name).ThenBy(s => s.LastName);
+
             while (true)
             {
-                // Читаем введенный с консоли символ
                 var input = Console.ReadKey().KeyChar;
 
-                // проверяем, число ли это
                 var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
 
-                // если не соответствует критериям - показываем ошибку
                 if (!parsed || pageNumber < 1 || pageNumber > 3)
                 {
                     Console.WriteLine();
                     Console.WriteLine("Страницы не существует");
                 }
-                // если соответствует - запускаем вывод
                 else
                 {
-                    // пропускаем нужное количество элементов и берем 2 для показа на странице
-                    var pageContent = phoneBook.Skip((pageNumber - 1) * 2).Take(2);
+                    var pageContent = sortedName.Skip((pageNumber - 1) * 2).Take(2);
                     Console.WriteLine();
 
-                    // выводим результат
-                    foreach (var entry in pageContent)
+                foreach (var entry in pageContent)
                         Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
 
                     Console.WriteLine();
